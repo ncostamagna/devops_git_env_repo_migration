cbad0aae16d9100d4519cd7832da9b12e1e81bd2
Nahuel <nlcostamagna@gmail.com>
2023-05-24 19:02:54 -0300

    feat: gitlab export and github import (#1)

--- github_api.py
+++ github_api.py
@@ -3,12 +3,14 @@ import time
 from decouple import config
 import types
 import sys
-
+from base64 import b64encode
+from nacl import encoding, public
 
 __TOKEN = "Bearer " + config('GITHUB_TOKEN')
 __HEADER = {"Content-Type": "application/json; charset=utf-8", 'Authorization': __TOKEN}
 __HOST = "https://api.github.com"
 __ENVS = config("ENVS").split(',')
+__KEY = config("SECRET_KEY")
 
 def get_repository_id(name):
         url = f'{__HOST}/repos/{name}'
@@ -19,6 +21,54 @@ def get_repository_id(name):
             sys.exit(f"GUTHUB_GET_REPOSITORY:\n {res} \n URL: {url}")
         return res['id']
 
+
+def get_variables(repository):
+        variables = []
+       
+        page = 1
+        max_page = 10
+        end_pagination = False
+        while end_pagination == False:
+            url =  url = f'{__HOST}/repos/{repository}/actions/variables?page={page}'
+            print(url)
+            s = requests.get(url, headers=__HEADER)
+            res = s.json()
+
+            if s.status_code > 299:
+                sys.exit(f"GUTHUB_GET_VARIABLES:\n {res} \n URL: {url}")
+
+            if res['total_count'] > page * max_page:
+                page = page + 1
+            else:
+                end_pagination = True
+            res = [dict(r, env="*") for r in res['variables']]
+            variables = variables + res
+                
+        return variables
+
+
+def get_secrets(repository):
+        secrets = []
+        page = 1
+        max_page = 30
+        end_pagination = False
+        while end_pagination == False:
+            url =  url = f'{__HOST}/repos/{repository}/actions/secrets?page={page}'
+            print(url)
+            s = requests.get(url, headers=__HEADER)
+            res = s.json()
+
+            if s.status_code > 299:
+                sys.exit(f"GUTHUB_GET_SECRETS:\n {res} \n URL: {url}")
+            
+            if res['total_count'] > page * max_page:
+                page = page + 1
+            else:
+                end_pagination = True
+            
+            res = [dict(r, env="*") for r in res['secrets']]
+            secrets = secrets + res
+        return secrets
 
 def get_variables_by_env(repo_id):
         variables = []
@@ -33,7 +83,7 @@ def get_variables_by_env(repo_id):
                 res = s.json()
 
                 if s.status_code > 299:
-                    sys.exit(f"GUTHUB_GET_REPOSITORY:\n {res} \n URL: {url}")
+                    sys.exit(f"GUTHUB_GET_VARIABLES_BY_ENV:\n {res} \n URL: {url}")
 
                 if res['total_count'] > page * max_page:
                     page = page + 1
@@ -57,7 +107,7 @@ def get_secrets_by_env(repo_id):
                 res = s.json()
 
                 if s.status_code > 299:
-                    sys.exit(f"GUTHUB_GET_REPOSITORY:\n {res} \n URL: {url}")
+                    sys.exit(f"GUTHUB_GET_SECRETS_BY_ENV:\n {res} \n URL: {url}")
                 
                 if res['total_count'] > page * max_page:
                     page = page + 1
@@ -67,3 +117,68 @@ def get_secrets_by_env(repo_id):
                 res = [dict(r, env=env) for r in res['secrets']]
                 secrets = secrets + res
         return secrets
+    
+def get_public_key(repo_name):
+    url = f"{__HOST}/repos/{repo_name}/actions/secrets/public-key"
+    print(url)
+    s = requests.get(url, headers=__HEADER)
+
+    res = s.json()
+    if s.status_code > 299:
+        sys.exit(f"GUTHUB_CREATE_VARIABLE:\n {res} \n URL: {url}")
+
+    return  res
+
+def get_public_key_by_env(repo_id):
+    env_key = {}
+
+    for env in __ENVS:
+        url = f"{__HOST}/repositories/{repo_id}/environments/{env}/secrets/public-key"
+        print(url)
+        s = requests.get(url, headers=__HEADER)
+        res = s.json()
+        if s.status_code > 299:
+            sys.exit(f"GITHUB_GET_PUBLIC_KEY_BY_ENV:\n {res} \n URL: {url}")
+        
+        env_key[env] = res
+
+    return env_key
+
+
+def create_variable(repo_name, repo_id, key, value, env):
+    url = f"repositories/{repo_id}/environments/{env}" if env != None else f"repos/{repo_name}/actions"
+    url = f"{__HOST}/{url}/variables"
+    print(url, key)
+    data = {"name": key, "value": value}
+    s = requests.post(url, json=data, headers=__HEADER)
+
+    res = s.json()
+    if s.status_code > 299:
+        sys.exit(f"GUTHUB_CREATE_VARIABLE:\n {res} \n URL: {url}")
+
+    return
+
+def create_secret(repo_name, repo_id, key, value, env, public_key_id, public_key_value):
+
+    url = f"repositories/{repo_id}/environments/{env}" if env != None else f"repos/{repo_name}/actions"
+    url = f"{__HOST}/{url}/secrets/{key}"
+
+    enc_value = encrypt(public_key_value, value)
+    data = {'key_id': public_key_id, "encrypted_value":enc_value}
+    
+    print(url, key)
+    s = requests.put(url, json=data, headers=__HEADER)
+
+    res = s.json()
+    if s.status_code > 299:
+        sys.exit(f"GUTHUB_CREATE_SECRET:\n {res} \n URL: {url}")
+
+    return
+
+
+def encrypt(public_key: str, secret_value: str) -> str:
+  """Encrypt a Unicode string using the public key."""
+  public_key = public.PublicKey(public_key.encode("utf-8"), encoding.Base64Encoder())
+  sealed_box = public.SealedBox(public_key)
+  encrypted = sealed_box.encrypt(secret_value.encode("utf-8"))
+  return b64encode(encrypted).decode("utf-8")
--- github_sdk.py
+++ github_sdk.py
@@ -13,20 +13,52 @@ def import_environment(replace, file):
     id = github_api.get_repository_id(GITHUB_REPO_NAME)
 
     # get vars
-    vars = github_api.get_variables_by_env(id)
-    var_df = pd.DataFrame.from_dict(vars)
+    vars_env = github_api.get_variables_by_env(id)
+    vars_rep = github_api.get_variables(GITHUB_REPO_NAME)
+    var_df = pd.DataFrame.from_dict(vars_env + vars_rep)
     var_df['secret'] = 0
 
     # get secrets
-    sec = github_api.get_secrets_by_env(id)
-    sec_df = pd.DataFrame.from_dict(sec)
+    sec_env = github_api.get_secrets_by_env(id)
+    sec_rep = github_api.get_secrets(GITHUB_REPO_NAME)
+    sec_df = pd.DataFrame.from_dict(sec_env + sec_rep)
     sec_df['secret'] = 1
-
     
     # remove import data that existing in github
-    df_check = pd.concat([var_df, sec_df],axis=0)[['name', 'value', 'env', 'secret']]
-    df = df_import.merge(df_check, left_on=['key', 'env', 'secret'], right_on=['name', 'env', 'secret'], suffixes=('_import', '_github'),  how='left', indicator=True)
-    df = df.loc[df['_merge'] == 'left_only',['key', 'value_import', 'env', 'secret']]
-    print(df)
-
+    df_check = pd.concat([var_df, sec_df],axis=0)
+    df = None
+    if df_check.empty == False:
+        df_check = pd.concat([var_df, sec_df],axis=0)[['name', 'env', 'secret']]
+        df = df_import.merge(df_check, left_on=['key', 'env', 'secret'], right_on=['name', 'env', 'secret'], suffixes=('_import', '_github'),  how='left', indicator=True)
+        df = df.loc[df['_merge'] == 'left_only',['key', 'value', 'env', 'secret']]
+    else:
+        df = df_import[['key', 'value', 'env', 'secret']]
     
+
+    print(df, '\n')
+    print(df['env'].value_counts(), '\n')
+    print(df['secret'].value_counts(), '\n')
+    user_input = input('Do you want to run the process (yes/no): ')
+
+    if not (user_input.lower() == 'yes' or  user_input.lower() == 'y'):  
+        print("exit")      
+        return
+
+    print("running variables and secrets:")
+    pkey = github_api.get_public_key(GITHUB_REPO_NAME)
+    pkey_env = github_api.get_public_key_by_env(id)
+
+    for index, row in df.iterrows():
+        env = row['env']
+        if row['secret'] == 1:
+            if env == "*":
+                github_api.create_secret(GITHUB_REPO_NAME, None, row['key'], row['value'], None, pkey['key_id'], pkey['key'])
+            else:
+                github_api.create_secret(None, id, row['key'], row['value'], env, pkey_env[env]['key_id'], pkey_env[env]['key'])
+        else:    
+            if env == "*":
+                github_api.create_variable(GITHUB_REPO_NAME, None, row['key'], row['value'], None)
+            else:
+                github_api.create_variable(None, id, row['key'], row['value'], env)
+    
+    print("The process has completed successfully")
